0ea5a4fc532ece584188bd9c66267925a5a8ee98
icyzy <shamanking_yoh_anna@hotmail.com>
2018-08-28 08:09:11 +0700

    got each link as array list

--- app/src/main/java/com/icyzy/niceoppai/ServiceHandler.java
+++ app/src/main/java/com/icyzy/niceoppai/ServiceHandler.java
@@ -9,6 +9,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 class ServiceHandler extends AsyncTask<Void, Void, Void> {
 
@@ -20,27 +21,34 @@ class ServiceHandler extends AsyncTask<Void, Void, Void> {
             try {
                 InputStreamReader in = new InputStreamReader(urlConnection.getInputStream());
                 BufferedReader r = new BufferedReader(in);
-                StringBuilder sb = new StringBuilder();
+                ArrayList<String> lines = new ArrayList<String>();
+                ArrayList<String> use = new ArrayList<String>();
+//                StringBuilder sb = new StringBuilder();
                 Integer i = 1;
                 try {
                     String s;
                     Boolean t = false;
                     while ((s = r.readLine()) != null) {
-                        if (s.contains("<div class=\"textwidget\">")) {
+                        if (s.contains("<div class=\"wpm_pag mng_lts_chp grp\">")) {
                             t = true;
-                        } else if (s.contains("/body>")) {
+                        } else if (s.contains("<div class=\"wpm_nav\">\n")) {
                             t = false;
                         }
-                        if (t) {
-                            sb.append(String.valueOf(i)+": "+s);
-                            sb.append("\n");
+                        if (t && !s.contains("</div>") && !s.contains("</a>") && !s.contains("</li>") && !s.contains("<li>") && !s.contains("</ul>") && (s.contains("href")) || s.contains("<img src")){
+                            lines.add(s);
+//                            sb.append("\n");
                             i++;
                         }
                     }
                 } finally {
                     r.close();
                 }
-                Log.d("Test", String.valueOf(sb));
+                for (int i1 = 9; i1 < lines.size(); i1++) {
+                    use.add(lines.get(i1));
+                }
+                for (String us: use) {
+                    Log.d("Test", us);
+                }
                 Log.d("Line", String.valueOf(i));
                 return null;
             } catch (Exception e) {
